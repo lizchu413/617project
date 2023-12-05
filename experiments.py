1d409000d16741fae406d290c7f9b96fd3b48dd6
Alex B Cheng <abcheng@patient.lti.cs.cmu.edu>
2023-12-05 03:57:38 -0500

    new experiment file

---
+++ experiments.py
@@ -0,0 +1,78 @@
+
+
+from codet5_to_qa_new import train, format_and_tokenize
+from datasets import load_dataset
+from transformers import TrainingArguments, AutoModelForSeq2SeqLM, AutoTokenizer
+import torch
+
+CODET5 = "Salesforce/codet5p-220m"
+BERT = "microsoft/codebert-base"
+BATCH_SIZE = 6
+EPOCHS = 8
+SAVE_EVERY = 1
+LR = 1e-4
+WORKERS = 2
+MAX_INPUT_LENGTH = 120
+SEED = 617
+
+
+def load_experiment(model_out: str, loss_out:str, lr=5e-5, weight_decay=0):
+    result = dict()
+    result["model_out"] = model_out
+    result["loss_out"] = loss_out
+    result["lr"] = lr
+    result["wd"] = weight_decay
+    return result
+
+
+
+def experiments(model_name, train_set, val_set, parameter_sets: list) -> None:
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    for parameters in parameter_sets:
+        args = TrainingArguments(
+            output_dir="codet5-to-qa", 
+            evaluation_strategy="epoch",
+            logging_strategy="epoch",
+            save_strategy="epoch",
+            learning_rate=parameters["lr"],
+            weight_decay = parameters["wd"],
+            per_device_train_batch_size=8,
+            per_device_eval_batch_size=4,
+            num_train_epochs=EPOCHS,
+            load_best_model_at_end=True,
+            fp16 = True
+        )
+        torch.cuda.empty_cache()
+        train(model, tokenizer, train_set, val_set, args, parameters["model_out"], parameters["loss_out"])
+
+
+
+if __name__ == "__main__":
+
+    # load in dataset
+
+    _data = load_dataset("aalexchengg/codesearchnet_qa", streaming = True)
+    train_set, val_set, test_set = format_and_tokenize(_data, num_train = 4000, num_val=100, num_test=100)
+
+    # hyperparams to tune
+    learning_rates = [5e-4,5e-5,5e-6]
+    weight_decay = [0.001, 0.0001]
+
+    # code t5 experiments
+
+    #generate experiments
+    t5_lr_experiments = [load_experiment("codet5_lr{}.model".format(str(lr).replace(".", "")), "codet5_lr{}.txt".format(str(lr).replace(".", "")), lr, 0) for lr in learning_rates]
+    t5_wd_experiments = [load_experiment("codet5_wd{}.model".format(str(wd).replace(".", "")), "codet5_lr{}.txt".format(str(wd).replace(".", "")), 5e-5, wd) for wd in weight_decay]
+    # run experiments
+    experiments(CODET5, train_set, val_set, t5_lr_experiments)
+    experiments(CODET5, train_set, val_set, t5_wd_experiments)
+
+    # codebert experiments
+
+    bert_lr_experiments = [load_experiment("bert_lr{}.model".format(str(lr).replace(".", "")), "bert_lr{}.txt".format(str(lr).replace(".", "")), lr, 0) for lr in learning_rates]
+    bert_wd_experiments = [load_experiment("bert_wd{}.model".format(str(wd).replace(".", "")), "bert_lr{}.txt".format(str(wd).replace(".", "")), 5e-5, wd) for wd in weight_decay]
+    # run experiments
+    experiments(CODET5, train_set, val_set, t5_lr_experiments)
+    experiments(CODET5, train_set, val_set, t5_wd_experiments)
+
